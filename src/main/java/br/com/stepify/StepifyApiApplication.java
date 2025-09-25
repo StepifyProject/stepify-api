a835ea8b8010cc4ee50d0f67791d92c31b01e941
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2025-09-25 16:55:32 -0300

    First commit

---
+++ src/main/java/br/com/stepify/StepifyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.stepify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepifyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/stepify/StepifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stepify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
