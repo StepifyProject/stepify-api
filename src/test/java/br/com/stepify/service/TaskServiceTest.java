fb4b7b4429681aaea58900caa67700138ba7fbb5
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2025-10-21 19:13:30 -0300

    test: add unit tests for findById, updateById, and deleteById methods in TaskService

--- src/test/java/br/com/stepify/service/TaskServiceTest.java
+++ src/test/java/br/com/stepify/service/TaskServiceTest.java
@@ -2,25 +2,33 @@ package br.com.stepify.service;
 
 import br.com.stepify.command.microtask.inputs.CreateMicroTaskCommand;
 import br.com.stepify.command.task.inputs.CreateTaskCommand;
+import br.com.stepify.command.task.inputs.UpdateTaskCommand;
 import br.com.stepify.command.task.outputs.TaskDTO;
 import br.com.stepify.enums.ETaskPriority;
 import br.com.stepify.enums.ETaskStatus;
+import br.com.stepify.exception.EntityNotFoundException;
 import br.com.stepify.mapper.TaskMapper;
 import br.com.stepify.mongo.entity.MicroTask;
 import br.com.stepify.mongo.entity.Task;
 import br.com.stepify.mongo.repository.TaskRepository;
 import br.com.stepify.service.action.task.UpdateTaskAction;
+import com.mongodb.client.result.UpdateResult;
+import org.bson.Document;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -42,7 +50,7 @@ class TaskServiceTest {
         @Test
         void shouldCreateTaskSuccessfullyWithEmptyMicroTasks() {
             CreateTaskCommand command = createTaskCommand(Collections.emptyList());
-            Task expectedTask = createTask(command, Collections.emptyList());
+            Task expectedTask = createTask("1", command, Collections.emptyList());
             TaskDTO expectedTaskDTO = createTaskDTO(expectedTask);
 
             when(taskMapper.fromCommand(any(CreateTaskCommand.class))).thenReturn(expectedTask);
@@ -69,7 +77,7 @@ class TaskServiceTest {
             CreateMicroTaskCommand microTaskCommand = createMicroTaskCommand();
             CreateTaskCommand taskCommand = createTaskCommand(List.of(microTaskCommand));
             MicroTask expectedMicroTask = createMicroTask(microTaskCommand);
-            Task expectedTask = createTask(taskCommand, List.of(expectedMicroTask));
+            Task expectedTask = createTask("1", taskCommand, List.of(expectedMicroTask));
             TaskDTO expectedTaskDTO = createTaskDTO(expectedTask);
 
             when(taskMapper.fromCommand(any(CreateTaskCommand.class))).thenReturn(expectedTask);
@@ -139,6 +147,115 @@ class TaskServiceTest {
         }
     }
 
+    @Nested
+    class FindTaskById {
+        @Test
+        void shouldReturnTaskSuccessfullyWhenExists() {
+            String taskId = "1";
+            Task expectedTask = createTask(taskId, Collections.emptyList());
+            TaskDTO expectedTaskDTO = createTaskDTO(expectedTask);
+
+            when(taskRepository.findByIdAndDeletedFalse(taskId)).thenReturn(Optional.of(expectedTask));
+            when(taskMapper.toDTO(expectedTask)).thenReturn(expectedTaskDTO);
+
+            TaskDTO result = taskService.findTaskById(taskId);
+
+            assertNotNull(result);
+            assertEquals(expectedTask.getTitle(), result.title());
+            assertEquals(expectedTask.getDescription(), result.description());
+            assertEquals(expectedTask.getPriority(), result.priority());
+            assertEquals(expectedTask.getStatus(), result.status());
+            assertEquals(expectedTask.getDueDate(), result.dueDate());
+
+            verify(taskRepository).findByIdAndDeletedFalse(taskId);
+            verify(taskMapper).toDTO(expectedTask);
+        }
+
+        @Test
+        void shouldThrowEntityNotFoundExceptionWhenTaskDoesNotExists() {
+            when(taskRepository.findByIdAndDeletedFalse(anyString())).thenReturn(Optional.empty());
+
+            assertThrows(EntityNotFoundException.class, () -> taskService.findTaskById("1"));
+        }
+    }
+
+    @Nested
+    class UpdateTaskById {
+        @Test
+        void shouldUpdateTaskSuccessfullyWhenExists() {
+            String taskId = "1";
+            UpdateTaskCommand command = createUpdateTaskCommand();
+            Task existingTask = createTask(taskId, Collections.emptyList());
+            Task updatedTask = createTask(command, Collections.emptyList());
+            TaskDTO expectedTaskDTO = createTaskDTO(updatedTask);
+
+            when(taskRepository.findByIdAndDeletedFalse(taskId)).thenReturn(Optional.of(existingTask));
+            doNothing().when(updateTaskAction).execute(existingTask, command);
+            when(taskRepository.save(existingTask)).thenReturn(updatedTask);
+            when(taskMapper.toDTO(existingTask)).thenReturn(expectedTaskDTO);
+
+            TaskDTO result = taskService.updateTaskById(taskId, command);
+
+            assertNotNull(result);
+            assertEquals(command.title(), result.title());
+            assertEquals(command.description(), result.description());
+            assertEquals(command.priority(), result.priority());
+            assertEquals(command.status(), result.status());
+            assertEquals(command.dueDate(), result.dueDate());
+            assertEquals(command.completedAt(), result.completedAt());
+
+            verify(taskRepository).findByIdAndDeletedFalse(taskId);
+            verify(updateTaskAction).execute(existingTask, command);
+            verify(taskRepository).save(existingTask);
+            verify(taskMapper).toDTO(existingTask);
+        }
+
+        @Test
+        void shouldThrowEntityNotFoundExceptionWhenTaskDoesNotExists() {
+            UpdateTaskCommand command = createUpdateTaskCommand();
+
+            when(taskRepository.findByIdAndDeletedFalse(anyString())).thenReturn(Optional.empty());
+
+            assertThrows(EntityNotFoundException.class, () -> taskService.updateTaskById("1", command));
+        }
+    }
+
+    @Nested
+    class DeleteTaskById {
+        @Test
+        void shouldDeleteTaskSuccessfullyWhenExists() {
+            String taskId = "1";
+            Task expectedTask = createTask(taskId, Collections.emptyList());
+            UpdateResult updateResult = mock(UpdateResult.class);
+
+            ArgumentCaptor<Query> queryCaptor = ArgumentCaptor.forClass(Query.class);
+            ArgumentCaptor<Update> updateCaptor = ArgumentCaptor.forClass(Update.class);
+
+            when(taskRepository.findByIdAndDeletedFalse(taskId)).thenReturn(Optional.of(expectedTask));
+            when(mongoTemplate.updateFirst(any(Query.class), any(Update.class), eq(Task.class))).thenReturn(updateResult);
+
+            assertDoesNotThrow(() -> taskService.deleteTaskById(taskId));
+
+            verify(taskRepository).findByIdAndDeletedFalse(taskId);
+            verify(mongoTemplate).updateFirst(queryCaptor.capture(), updateCaptor.capture(), eq(Task.class));
+
+            Query capturedQuery = queryCaptor.getValue();
+            assertEquals(taskId, capturedQuery.getQueryObject().get("id"));
+
+            Update capturedUpdate = updateCaptor.getValue();
+            assertEquals(true, capturedUpdate.getUpdateObject().get("$set", Document.class).get("deleted"));
+        }
+
+        @Test
+        void shouldThrowEntityNotFoundExceptionWhenTaskDoesNotExists() {
+            when(taskRepository.findByIdAndDeletedFalse(anyString())).thenReturn(Optional.empty());
+
+            assertThrows(EntityNotFoundException.class, () -> taskService.deleteTaskById("1"));
+
+            verify(mongoTemplate, never()).updateFirst(any(Query.class), any(Update.class), eq(Task.class));
+        }
+    }
+
     private CreateTaskCommand createTaskCommand(List<CreateMicroTaskCommand> microTaskCommands) {
         return createTaskCommand("Title", microTaskCommands);
     }
@@ -167,8 +284,15 @@ class TaskServiceTest {
         );
     }
 
-    private Task createTask(CreateTaskCommand command, List<MicroTask> microTasks) {
-        return createTask("1", command, microTasks);
+    private UpdateTaskCommand createUpdateTaskCommand() {
+        return new UpdateTaskCommand(
+                "Title Updated",
+                "Description Updated",
+                ETaskStatus.IN_PROGRESS,
+                ETaskPriority.LOW,
+                LocalDateTime.now().plusDays(7),
+                LocalDateTime.now()
+        );
     }
 
     private Task createTask(String id, CreateTaskCommand command, List<MicroTask> microTasks) {
@@ -186,6 +310,36 @@ class TaskServiceTest {
                 .build();
     }
 
+    private Task createTask(String id, List<MicroTask> microTasks) {
+        return Task.builder()
+                .id(id)
+                .title("Task " + id)
+                .description("Description " + id)
+                .status(ETaskStatus.PENDING)
+                .priority(ETaskPriority.MEDIUM)
+                .dueDate(LocalDateTime.now().plusDays(3))
+                .microTasks(microTasks)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .deleted(false)
+                .build();
+    }
+
+    private Task createTask(UpdateTaskCommand command, List<MicroTask> microTasks) {
+        return Task.builder()
+                .id("1")
+                .title(command.title())
+                .description(command.description())
+                .status(command.status())
+                .priority(command.priority())
+                .dueDate(command.dueDate())
+                .completedAt(command.completedAt())
+                .microTasks(microTasks)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .deleted(false)
+                .build();
+    }
 
     private MicroTask createMicroTask(CreateMicroTaskCommand microTaskCommand) {
         return MicroTask.builder()
